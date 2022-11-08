615015d8165fc5d2c1c9d2b699daf31a6630a701
FikretDuzceker <fikretduzceker5@gmail.com>
2022-11-08 10:38:19 +0300

    restcountry4

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Header = () => {
+  return <div className="Header">World Countries</div>;
+};
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const Main = () => {
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState("");
+  // const [region, setRegion] = useState(null);
+
+  // const regionNames = [ "Africa", "Americas", "Asia", "Europe", "Oceania"];
+
+  const getCountries = async () => {
+    if (query !== "") {
+      const response = await axios.get(
+        `https://restcountries.com/v3.1/name/${query}`
+      );
+      setCountries(response.data);
+    } else {
+      alert("Please enter a country");
+    }
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getCountries();
+    setQuery("");
+  };
+
+  return (
+    <>
+      <div className="searchbar">
+        <form onSubmit={handleSubmit} className="searchform">
+          <input
+            type=""
+            placeholder="Enter country name..."
+            onChange={handleChange}
+            value={query}
+            autoFocus
+          />
+
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className="Countrylist">
+        {countries.map((country, index) => {
+          return (
+            <div key={index} className="countrylist-item">
+              <div className="flag">
+                {" "}
+                <img src={country.flags.png} />
+              </div>
+              <div className="countryinfo">
+                <p className="countryname">{country.name.official}</p>
+                <p className="countrycapital">{country.capital}</p>
+                <p className="countrpopulation">{country.population}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Main;
